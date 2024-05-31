60dd284e183e623a9e56495bcd7a9f86095f4a1b
Ishwar Patel <152163670+ishupatel07@users.noreply.github.com>
2024-05-31 12:36:04 +0530

    Time: 61 ms (86.5%), Space: 52.7 MB (73.24%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.js
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.js
@@ -2,10 +2,14 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// var buildArray = function(nums) {
+//     let arr = []
+//     for(let i = 0; i < nums.length; i++){
+//         arr.push(nums[nums[i]]) 
+//     }
+//     return arr
+// };
+
 var buildArray = function(nums) {
-    let arr = []
-    for(let i = 0; i < nums.length; i++){
-        arr.push(nums[nums[i]]) 
-    }
-    return arr
-};
+    return nums.map((n) => nums[n])
+}
