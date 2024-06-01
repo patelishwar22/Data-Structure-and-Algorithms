60c4858158f52a9f3f6ef62f30973bf0da9a19f0
Ishwar Patel <152163670+ishupatel07@users.noreply.github.com>
2024-06-01 14:01:20 +0530

    Time: 64 ms (80.25%), Space: 51.1 MB (78.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,10 +3,36 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-      const ss = s.split('').sort();
-        const tt = t.split('').sort();
+// var isAnagram = function(s, t) {
+//       const ss = s.split('').sort();
+//         const tt = t.split('').sort();
 
-        // Check if the sorted arrays are equal
-        return ss.join('') === tt.join('');
+//         // Check if the sorted arrays are equal
+//         return ss.join('') === tt.join('');
+// };
+
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const charCount = {};
+
+    for (let i = 0; i < s.length; i++) {
+        charCount[s[i]] = (charCount[s[i]] || 0) + 1;
+        charCount[t[i]] = (charCount[t[i]] || 0) - 1;
+    }
+
+    for (let key in charCount) {
+        if (charCount[key] !== 0) {
+            return false;
+        }
+    }
+
+    return true;
 };
+
+// Example usage
+let string1 = "anagram";
+let string2 = "nagaram";
+console.log(isAnagram(string1, string2)); // true
