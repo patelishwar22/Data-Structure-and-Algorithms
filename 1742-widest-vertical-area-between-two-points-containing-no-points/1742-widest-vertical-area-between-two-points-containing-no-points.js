eac976d2102401fa1ef2c66004be58d17f1576b9
Ishwar Patel <152163670+ishupatel07@users.noreply.github.com>
2024-06-05 22:12:15 +0530

    Time: 141 ms (80.13%), Space: 71.5 MB (66.07%) - LeetHub

---
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/1742-widest-vertical-area-between-two-points-containing-no-points.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var maxWidthOfVerticalArea = function(points) {
+     points.sort((a, b) => a[0] - b[0]); // Sort by x-coordinates
+
+    let maxWidth = 0;
+    for (let i = 1; i < points.length; i++) {
+        maxWidth = Math.max(maxWidth, points[i][0] - points[i - 1][0]);
+    }
+
+    return maxWidth;
+};
