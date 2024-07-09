61ff15a685c24da8af1e6256ac3672b4f8f99292
Ishwar Patel <152163670+patelishwar22@users.noreply.github.com>
2024-07-09 15:24:10 +0530

    Time: 53 ms (76.17%), Space: 51.3 MB (34.47%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.js
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.js
@@ -5,9 +5,9 @@
 var mostWordsFound = function(sentences) {
     let max = 0
     for(let i = 0; i < sentences.length; i++){
-    let split = sentences[i].split(" ")
-    if(max < split.length){
-       max = split.length
+    let split = sentences[i].split(" ").length
+    if(max < split){
+       max = split
     }
     }
         return max
